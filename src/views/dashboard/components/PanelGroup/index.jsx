a429a1c6247d5de56ea4ebb528840568af11c27d
Liu Yi <ly@hcttop.com>
2021-02-04 17:04:52 +0800

    new： 首页的图表

--- src/views/dashboard/components/PanelGroup/index.jsx
+++ src/views/dashboard/components/PanelGroup/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Row, Col, Icon } from "antd";
-import CountUp from "react-countuo";
+import { Row, Col } from "antd";
+import  Icon  from "@ant-design/icons"
+import CountUp from "react-countup";
 import "./index.scss";
 
 const chartList = [
@@ -34,7 +35,36 @@ const chartList = [
       const { handleSetLineChartData } = props;
       return (
           <div className="panel-group-container">
-
+            <Row gutter={40} className="panel-group">
+                {
+                    chartList.map((chart, i) => (
+                        <Col
+                            key={i}
+                            lg={6}
+                            sm={12}
+                            xs={12}
+                            onClick={handleSetLineChartData.bind(this, chart.type)}
+                            className="card-panel-col"
+                        >
+                            <div className="card-panel">
+                                <div className="card-panel-icon-wrapper">
+                                    <Icon
+                                        className={chart.type}
+                                        style={{ fontSize: 55, color: chart.color }}
+                                        type={chart.icon}
+                                    />
+                                </div>
+                                <div className="card-panel-description">
+                                    <p className="card-panel-text">{ chart.type }</p>
+                                    <CountUp end={chart.num} start={0} className="card-panel-num" /> 
+                                </div>
+                            </div>
+                        </Col>
+                    ))
+                }
+            </Row>
           </div>
       )
   }
+
+  export default PanelGroup;
--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -7,11 +7,48 @@
  * Modified By: Liu Yi <ly@hcttop.com>
  */
 
-import React from "react"
+import React, { useState } from "react"
+import PanelGroup from "./components/PanelGroup";
+import LineChart from "./components/LineChart";
+
+const lineChartDefaultData = {
+    "New Visits": {
+      expectedData: [100, 120, 161, 134, 105, 160, 165],
+      actualData: [120, 82, 91, 154, 162, 140, 145],
+    },
+    Messages: {
+      expectedData: [200, 192, 120, 144, 160, 130, 140],
+      actualData: [180, 160, 151, 106, 145, 150, 130],
+    },
+    Purchases: {
+      expectedData: [80, 100, 121, 104, 105, 90, 100],
+      actualData: [120, 90, 100, 138, 142, 130, 130],
+    },
+    Shoppings: {
+      expectedData: [130, 140, 141, 142, 145, 150, 160],
+      actualData: [120, 82, 91, 154, 162, 140, 130],
+    },
+  };
 
 const Dashboard = () => {
+    const [lineChartData, setLineChartData] = useState(
+        lineChartDefaultData["New Visits"]
+      );
+    const handleSetLineChartData = (type) => setLineChartData(lineChartDefaultData[type]); 
+
     return (
-        <p>Dashboard</p>
+        <div className="app-container">
+            <p>Dashboard</p>
+            <PanelGroup handleSetLineChartData={handleSetLineChartData} />
+            <LineChart
+                chartData={lineChartData}
+                styles={{
+                padding: 12,
+                backgroundColor: "#fff",
+                marginBottom: "25px",
+                }}
+            />
+        </div>
     );
 }
 
