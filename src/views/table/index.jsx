a708264b13b8374c02611c50cda6fa0c8a44f833
Liu Yi <ly@hcttop.com>
2021-07-09 17:59:13 +0800

    #1、表单尝试

--- src/views/table/index.jsx
+++ src/views/table/index.jsx
@@ -3,19 +3,17 @@
  * Created Date: 2021-07-07 4:26:25 PM
  * Author: Liu Yi <ly@hcttop.com>
  * -----
- * Last Modified: 2021-07-09, 1:42:46 PM
+ * Last Modified: 2021-07-09, 5:47:43 PM
  * Modified By: Liu Yi <ly@hcttop.com>
  */
 
 import React from 'react'
-import { Table, Tag, Space } from 'antd'
+import { Table, Tag, Space, Form, Input,Select } from 'antd'
 import './index.scss'
 
 import data from './tableJson'
 
 const TableComponent = () => {
-    
-      
       const columns = [
         {
           title: '姓名',
@@ -66,6 +64,24 @@ const TableComponent = () => {
       ];
     return (
         <div className="header" id="head">
+            <Form
+            className="filter-form"
+            name="basic"
+            layout="inline"
+            >
+                <Form.Item 
+                name="title"
+                >
+                    <Input placeholder="标题" maxLength={200} />
+                </Form.Item>
+                <Form.Item name="importance">
+                    <Select>
+                        <Select.Option value="1">1</Select.Option>
+                        <Select.Option value="2">2</Select.Option>
+                        <Select.Option value="3">3</Select.Option>
+                    </Select>
+                </Form.Item>
+            </Form>
             <Table dataSource={data} columns={columns}  pagination={{ total: 30, showQuickJumper: true, showSizeChanger: true }} />;
         </div>
     )   
