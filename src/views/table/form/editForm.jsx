3c97d5bd7c3dedec93ad861e89e1c59d9a407c7d
Liu Yi <ly@hcttop.com>
2021-07-21 18:11:19 +0800

    #1、表格的查询翻页，以及图标使用

---
+++ src/views/table/form/editForm.jsx
@@ -0,0 +1,25 @@
+/*
+ * Description: reauired, the file description, reference link, etc.
+ * Created Date: 2021-07-21 4:46:30 PM
+ * Author: Liu Yi <ly@hcttop.com>
+ * -----
+ * Last Modified: 2021-07-21, 5:42:36 PM
+ * Modified By: Liu Yi <ly@hcttop.com>
+ */
+
+import React from 'react'
+import { Modal, Button } from 'antd';
+
+
+const EditForm = (visible) => {
+    return (
+        <Modal 
+            title="编辑"
+            visible={visible}
+        >
+            <p>111</p>
+        </Modal>
+    )
+}
+
+export default EditForm
--- src/views/table/index.jsx
+++ src/views/table/index.jsx
@@ -3,29 +3,29 @@
  * Created Date: 2021-07-07 4:26:25 PM
  * Author: Liu Yi <ly@hcttop.com>
  * -----
- * Last Modified: 2021-07-20, 4:17:51 PM
+ * Last Modified: 2021-07-21, 5:45:16 PM
  * Modified By: Liu Yi <ly@hcttop.com>
  */
 
 import React, { useEffect, useState } from 'react'
-import { Table, Space, Form, Input,Select, Button } from 'antd'
+import { Table, Tag, Divider, Form, Input,Select, Button } from 'antd'
 import './index.scss'
-
-import data from './tableJson'
-
+import { EditOutlined, DeleteOutlined  } from '@ant-design/icons';
 import { tableList } from "@/api/table"
-
+import  EditForm  from './form/editForm'
 
 const TableComponent = () => {
-    const [formData, setFormDate] = useState(data);
-    const [pageQuery, setPageQuery] = useState({
+    const [formData, setFormDate] = useState();
+    
+    const pageQuery = {
         pageNumber: 1,
         pageSize: 10,
         title: "",
         star: "",
         status:""
-    });
+    }
     const [total, setTotal] =  useState(0)
+    const [editModalVisible, setEditModalVisible] = useState(true)
     const [form] = Form.useForm();
     
     const columns = [
@@ -45,23 +45,37 @@ const TableComponent = () => {
         key: 'readings',
     },
     {
-        title: 'Tags',
+        title: '重要度',
         key: 'star',
         dataIndex: 'star',
     },
     {
         title: '状态',
         key: 'status',
-        dataIndex: 'status'
+        dataIndex: 'status',
+        render: (text) => { 
+                let color = text === "published" ? "green" : text === "deleted" ? "red" : ""; 
+                                
+                return ( <Tag color={color}>
+                    {text}
+                    </Tag>
+                )
+            }
+    },
+    {
+        title: '时间',
+        key: 'date',
+        dataIndex: 'date'
     },
     {
         title: 'Action',
         key: 'action',
         render: (text, record) => (
-            <Space size="middle">
-                <a href="/">Invite {record.name}</a>
-                <a href="/">Delete</a>
-            </Space>
+            <span>
+                <Button type="primary" shape="circle" icon={<EditOutlined />} title="编辑" />
+                <Divider type="vertical" />
+                <Button type="primary" shape="circle" icon={<DeleteOutlined />} title="删除" />
+            </span>
         )
     }
     ];
@@ -74,18 +88,17 @@ const TableComponent = () => {
             const list = response.data.data.items;
             console.log('list', list)
             const total = response.data.data.total;
+           
             setTotal(total)
-           setFormDate(list)
+            setFormDate(list)
         })
     }
     const onFinish = (values) => {
         console.log('Success:', values);
-        setPageQuery(
-            {
-                ...pageQuery,
-                title: values.title,
-            }
-        )
+      
+        pageQuery.title = values.title
+        pageQuery.status = values.status
+        pageQuery.star = values.star
         fecthData()
     };
 
@@ -93,12 +106,9 @@ const TableComponent = () => {
 
     // 分页变化触发
     const pageChange = (pagination) => {
-        setPageQuery(
-            {
-                ...pageQuery,
-                pageNumber: pagination,
-            }
-        )
+        console.log('pagination---->', pagination)
+        
+        pageQuery.pageNumber =  pagination.current
         fecthData()
     }
 
@@ -116,18 +126,17 @@ const TableComponent = () => {
                 >
                     <Input placeholder="标题" maxLength={200} />
                 </Form.Item>
-                <Form.Item name="importance" minLength={100}>
-                    <Select placeholder="重要性">
-                        <Select.Option value="1">1</Select.Option>
-                        <Select.Option value="2">2</Select.Option>
-                        <Select.Option value="3">3</Select.Option>
+                <Form.Item name="star" width={150}>
+                    <Select placeholder="推荐指数"  style={{ width: 120 }}>
+                        <Select.Option value={1}>★</Select.Option>
+                        <Select.Option value={2}>★★</Select.Option>
+                        <Select.Option value={3}>★★★</Select.Option>
                     </Select>
                 </Form.Item>
-                <Form.Item name="type" minLength={100}>
-                    <Select placeholder="类型">
-                        <Select.Option value="CN">CN</Select.Option>
-                        <Select.Option value="US">JP</Select.Option>
-                        <Select.Option value="EU">EU</Select.Option>
+                <Form.Item name="status" width={150}>
+                    <Select placeholder="类型"  style={{ width: 120 }}>
+                        <Select.Option value="published">published</Select.Option>
+                        <Select.Option value="draft">draft</Select.Option>
                     </Select>
                 </Form.Item>
                <Form.Item>
@@ -137,7 +146,9 @@ const TableComponent = () => {
                </Form.Item>
             </Form>
             <Table dataSource={formData} columns={columns} onChange={pageChange}  pagination={{ total: total, showQuickJumper: true, showSizeChanger: true } } />
+            <EditForm  visible={editModalVisible} />
         </div>
+      
     )   
 }
 
